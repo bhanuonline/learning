02a9d19d5393eb07094b6e85d701734895bef2be
Bhanu Pratap <bhanu.mmmec2013@gmail.com>
2024-05-19 20:04:44 +0530

    remove unused import

--- src/main/java/com/example/techdemo/TechdemoApplication.java
+++ src/main/java/com/example/techdemo/TechdemoApplication.java
@@ -3,8 +3,6 @@ package com.example.techdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Calendar;
-
 @SpringBootApplication
 public class TechdemoApplication {
 
